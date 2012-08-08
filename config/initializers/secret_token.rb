f4fbf5e43fd931ffe6cc89150638ccf91e5d1431
root <root@brokenwebapps.localdomain>
2012-08-08 15:23:12 -0400

    intitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyclone::Application.config.secret_token = 'd9d9076ce846a2bbb0bb5a2ac97cc9e27eca4f97bb6f4a4474f0cf5e781b833887360297fe1db6c0daff126b56a122aca9a8255f1566e90baa4f63617c383deb'
